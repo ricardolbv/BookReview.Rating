491eeb5981be2e3b87e3757e6aec53b9f4e9baa5
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2022-06-06 19:12:53 -0300

    feat: adding the ms consumer

--- BookReview.Rating.Infraestructure/EnvVariables.cs
+++ BookReview.Rating.Infraestructure/EnvVariables.cs
@@ -10,6 +10,6 @@ namespace BookReview.Rating.Infraestructure
     {
         public static string TOPIC_BUS => "bookclassificationmessage";
         public static string SERVICE_BUS_STRING => "Endpoint=sb://book-review.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Rq591Auu4s/G1d1Pj9Fxl8mTWVZBDD2BVFRMhUCpR2I=";
-        public static string SUBSCRIPTION_NAME => "book-rating";
+        public static string SUBSCRIPTION_NAME => "BookReview.Rating";
     }
 }
